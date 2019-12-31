b8a9717d7258e58ba7d94b277eedaab73342d212
Vasileios Michailidis <59122717+VMichailidis@users.noreply.github.com>
2019-12-31 20:33:40 +0200

    Update TSP presentation.py

--- TSP presentation.py
+++ TSP presentation.py
@@ -30,14 +30,9 @@ button_colour = square_colour
 method1_colour = "blue"
 method2_colour = "red"
 method3_colour = "green"
-t1 = 0
-d1 = 0
 
-t2 = 0
-d2 = 0
 
-t3 = 0
-d3 = 0
+
 
 tab_height = 75
 tab_width = 145
@@ -48,25 +43,46 @@ class Presentation():
 	''' The window in which the presentation is housed'''
 	def __init__(self, root):
 		root.title("Presentation")
-		root.wm_geometry("1000x500")
+		root.wm_geometry("1000x1000")
+		self.variables()
 		self.widgets()
+
+	def variables(self):
+		self.t1 = tk.StringVar()
+		self.t1.set("time:\n0")
+		self.d1 = tk.StringVar()
+		self.d1.set("distance\n0")
+
+		self.t2 = tk.StringVar()
+		self.t2.set("time:\n0")
+		self.d2 = tk.StringVar()
+		self.d2.set("distance\n0")
+
+		self.t3 = tk.StringVar()
+		self.t3.set("time:\n0")
+		self.d3 = tk.StringVar()
+		self.d3.set("distance\n0")
+
+		self.time_var = [self.t1, self.t2, self.t3]
+		self.dist_var = [self.d1, self.d2, self.d3]
+
 	def widgets(self):
 #Frames
 		bar = tk.Frame(root, bg = bar_colour)
 		bar.place(relwidth = 1, height = 100, x = 0, y = 0)
 		sidebar = tk.Frame(root, bg = square_colour)
 		sidebar.place(width = 155, relheight = 1, x = 0, y = 100)
-		square= tk.Frame(root, bg = square_colour)
+		square = tk.Frame(root, bg = square_colour)
 		square.place(x= 155, y = 100, relheight = 1, relwidth = 1)
 #Canvas
 		self.cartesian = tk.Canvas(square)
 		self.cartesian.place(width = 1100, height = 650, x = 50, y = 50)
 #bar Buttons
-		method1 = tk.Button(bar, bd = 0, bg = button_colour, text = "method 1", command = lambda: self.method_1(li))
+		method1 = tk.Button(bar, bd = 0, bg = button_colour, text = "method 1", command = lambda: self.method_1())
 		method1.place(height = tab_height, width = tab_width, y = 25, x = bt_pos(0))
 		method2 = tk.Button(bar, bd = 0, bg = button_colour, text = "method 2")
 		method2.place(height = tab_height, width = tab_width, y = 25, x = bt_pos(1))
-		method3 = tk.Button(bar, bd = 0, bg = button_colour, text = "method 3", command =lambda: self.connect(li, method3_colour))
+		method3 = tk.Button(bar, bd = 0, bg = button_colour, text = "method 3", command =lambda: self.connect(li, method3_colour, 3))
 		method3.place(height = tab_height, width = tab_width, y = 25, x = bt_pos(2))
 		gen10 = tk.Button(bar, bd = 0, bg = button_colour, text = "generate 10 points",command = lambda: self.show_points(10))
 		gen10.place(height = tab_height, width = tab_width, y = 25, x = bt_pos(3))
@@ -76,26 +92,29 @@ class Presentation():
 		clear = tk.Button(bar, bd = 0,  bg = button_colour, text = "clear", command = self.clear)
 		clear.place(height = tab_height, width = tab_width, y = 25, x = bt_pos(5))
 #Results
+	#method 1
 		method1_label = tk.Label(sidebar, bg = method1_colour, text="mehtod 1")
 		method1_label.place(width = tab_width, height = 75, y = sd_pos(0), x = 5)
-		method1_time = tk.Label(sidebar, bg = method1_colour, text = "time:\n{}".format(t1))
-		method1_time.place(width = tab_width, height = 75, y = sd_pos(1), x = 5)
-		method1_distance = tk.Label(sidebar, bg = method1_colour, text = "distance:\n{}".format(d1))
-		method1_distance.place(width = tab_width, height = 75, y = sd_pos(2), x = 5)
-		
+		self.method1_time = tk.Label(sidebar, bg = method1_colour, textvariable = self.t1)
+		self.method1_time.place(width = tab_width, height = 75, y = sd_pos(1), x = 5)
+		self.method1_distance = tk.Label(sidebar, bg = method1_colour, textvariable = self.d1)
+		self.method1_distance.place(width = tab_width, height = 75, y = sd_pos(2), x = 5)
+	#method 2
 		method2_label = tk.Label(sidebar, bg = method2_colour, text="mehtod 2")
 		method2_label.place(width = tab_width, height = 75, y = sd_pos(3), x = 5)
-		method2_time = tk.Label(sidebar, bg = method2_colour, text = "time:\n{}".format(t2))
-		method2_time.place(width = tab_width, height = 75, y = sd_pos(4), x = 5)
-		method2_distance = tk.Label(sidebar, bg = method2_colour, text = "distance:\n{}".format(d2))
-		method2_distance.place(width = tab_width, height = 75, y = sd_pos(5), x = 5)
-
+		self.method2_time = tk.Label(sidebar, bg = method2_colour, textvariable = self.t2)
+		self.method2_time.place(width = tab_width, height = 75, y = sd_pos(4), x = 5)
+		self.method2_distance = tk.Label(sidebar, bg = method2_colour,textvariable = self.d2)
+		self.method2_distance.place(width = tab_width, height = 75, y = sd_pos(5), x = 5)
+	#method 3
 		method3_label = tk.Label(sidebar, bg = method3_colour, text="method 3")
 		method3_label.place(width = tab_width, height = 75, y = sd_pos(6), x = 5)
-		self.method3_time = tk.Label(sidebar, bg = method3_colour, text = "time:\n{}".format(t3))
+		self.method3_time = tk.Label(sidebar, bg = method3_colour, textvariable = self.t3)
 		self.method3_time.place(width = tab_width, height = 75, y = sd_pos(7), x = 5)
-		self.method3_distance = tk.Label(sidebar, bg = method3_colour, text = "distance:\n{}".format(d3))
+		self.method3_distance = tk.Label(sidebar, bg = method3_colour, textvariable = self.d3)
 		self.method3_distance.place(width = tab_width, height = 75, y = sd_pos(8), x = 5)
+
+
 	def load_points(self, n):
 		points = []
 		for i in range(n):
@@ -111,45 +130,49 @@ class Presentation():
 			x = i.x
 			y = i.y
 			self.cartesian.create_oval(x-1,y-1, x+1, y+1, fill = "black")
-	def connect(self,li, c):
+	def connect(self,l, c, v):
 		sumdis = 0
 		try:
 			t0 = dt.time()
-			for i in range(len(li)-1):
-				x1 = li[i].x
-				y1 = li[i].y
-				x2 = li[i+1].x
-				y2 = li[i+1].y
-				d = li[i].distance(li[i+1])
+			for i in range(len(l)-1):
+				x1 = l[i].x
+				y1 = l[i].y
+				x2 = l[i+1].x
+				y2 = l[i+1].y
+				d = l[i].distance(l[i+1])
 				sumdis +=d
 				self.cartesian.create_line(x1,y1,x2,y2, fill = c)
-			x1 = li[0].x
-			y1 = li[0].y
-			x2 = li[-1].x
-			y2 = li[-1].y
-			d = li[0].distance(li[-1])
+			x1 = l[0].x
+			y1 = l[0].y
+			x2 = l[-1].x
+			y2 = l[-1].y
+			d = l[0].distance(l[-1])
 			sumdis+=d
 			d = round(d, 2)
 			t = dt.time() - t0
 			t = round(t, 2)
 			self.cartesian.create_line(x1,y1,x2,y2, fill = c)
 
-			self.method3_distance.configure(text = "distance:\n{}".format(d))
-			self.method3_time.configure(text = "time:\n{} s".format(t))
+			self.dist_var[v-1].set("distance:\n{}".format(d))
+			self.time_var[v-1].set("time:\n{}".format(t))
+
 		except:
 			d = 0
 			t = 0
-			self.method3_distance.configure(text = "distance:\n{}".format(d))
-			self.method3_time.configure(text = "time:\n{}".format(0))
+			self.d3.set("distance:\n{}".format(d))
+			self.t3.set("time:\n{}".format(t))
+		print("d:"+str(d))
 	def clear(self):
 		global li
 		self.cartesian.delete("all")
 		li = []
 
-	def method_1(self, li):
+	def method_1(self):
 		matrix = []
-		r = []
+		
+		meth1sol = []
 		for i in li:
+			r = []
 			for j in li:
 				r.append(i.distance(j))
 			matrix.append(r)
@@ -190,7 +213,8 @@ class Presentation():
 		pm.end()
 		for i in subt:
 			meth1sol.append(li[i])
-		self.connect(meth1sol, method1_colour)
+		self.connect(meth1sol, method1_colour, 1)
+		
 
 
 root = tk.Tk()
